bdcdda73a857b0297e4a50038ee9eb6f0e37a1ef
Limin Shen <limin.shen@hotmail.com>
2012-06-09 18:20:18 -0500

    add view engine and templates

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
-var express = require('express');
+// npm
+const express = require('express');
+const hbs = require('hbs');
+const _ = require('underscore');
+
+
 
 var app = express.createServer();
 
+// Configuration
 app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-    app.use(app.router);
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
 });
+
+// setup view engine
+express.view.register('.hbs', hbs);
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'hbs');
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+  if (!block) {
+      block = blocks[name] = [];
+  }
+
+  block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+
+  // clear the block
+  blocks[name] = [];
+  return val;
+});
+
+// session support
+app.use(express.cookieParser());
+app.use(express.session({ secret: "ontheside" }));
+
 
 app.configure('development', function(){
     app.use(express.static(__dirname + '/public')); //dir of the currently executing script.
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.render('index', {
+    title : 'Give Pulse'
+  });
 });
 
 
