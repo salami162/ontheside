2129cbe52021bae28917a8292cae635f6a13fb89
Limin Shen <limin.shen@hotmail.com>
2013-05-23 17:11:55 -0700

    make server running

--- server.js
+++ server.js
@@ -2,45 +2,40 @@
 var express = require('express');
 var _ = require('underscore');
 
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./server/routes')
+  , http = require('http')
+  , path = require('path');
 
 var app = express();
 app.enable("jsonp callback");
 
-// Configuration
-app.configure(function(){
-  app.set('title', 'Limin Shen');
-  app.set('views', __dirname + '/server/views');
-  app.set('view engine', 'jade');
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+//app.use(app.router);
+app.use(express.static( path.join(__dirname, 'public') ));
 
-// setup server routes
-var routes = require('./server/routes');
-var api = require('./server/routes/api');
-
-
-// set jade as template engine
-// app.engine('jade', require('jade').__express);
-
-// session support
-app.use(express.cookieParser());
-app.use(express.session({ secret: "ontheside" }));
-
-
-app.configure('development', function(){
-    app.use(express.static(__dirname + '/public')); //dir of the currently executing script.
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
+// development only
+if ('development' === app.get('env')) {
+  app.use( express.errorHandler({ dumpExceptions: true, showStack: true }) );
+}
 
 app.get('/', routes.index);
-api.routes('/api', app);
 
-app.listen(1234);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server running @ http://127.0.0.1 on port ' + app.get('port'));
+});
 
-console.log('Server running @ http://127.0.0.1:1234');
+
 
 
