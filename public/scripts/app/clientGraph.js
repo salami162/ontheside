41d4b38f43766d0786ebae5f2678b8d276034334
Limin Shen <limin.shen@hotmail.com>
2013-05-27 22:30:43 -0700

    use backbone view

--- public/scripts/app/clientGraph.js
+++ public/scripts/app/clientGraph.js
@@ -1,8 +1,9 @@
 define([
   'vendor/backbone',
   'vendor/jquery',
-  'vendor/underscore'
-], function (Backbone, $, _) {
+  'vendor/underscore',
+  'vendor/d3.v3'
+], function (Backbone, $, _, d3) {
   var clientGraphModel = Backbone.Model.extend({
     name : 'clientGraph',
 
@@ -10,6 +11,7 @@ define([
       this.set({
         baseUrl : 'http://localhost:3000/clientGraph'
       });
+      this.listenTo(this, 'fetchClientGraph', this.fetch);
     },
 
     setFilters : function (filters) {
@@ -28,21 +30,107 @@ define([
       return fetchUrl + '?' + paramString.substring(0, paramString.length - 1);
     },
 
-    fetch : function () {
-      var fetchUrl = this.url();
+    fetch : function (filters) {
+      var self = this;
+      this.setFilters(filters);
 
-      var fetchData = $.ajax({
-        url : fetchUrl,
+      var fetchRequest = $.ajax({
+        url : this.url(),
         dataType : 'jsonp',
         async : true,
         cache : false,
         timeout : 20000
       });
 
-      return fetchData;
+      fetchRequest.done(function (data) {
+        self.trigger('drawGraph', data);
+      })
+      .fail(function (error) {
+        self.trigger('showError', error);
+      });
     }
-
   });
 
-  return clientGraphModel;
+  var clientGraphView = Backbone.View.extend({
+    name : 'clientGraph',
+    width : 1000,
+    height : 600,
+
+    events: {
+      'click button#update-chart': 'updateChart'
+    },
+
+    initialize : function (options) {
+      Backbone.View.prototype.initialize.apply(this, arguments);
+      this.svgChart = d3.select('svg')
+                      .attr('width', this.width)
+                      .attr('height', this.height);
+      this.listenTo(this.model, 'drawGraph', this.drawGraph);
+    },
+
+    updateChart : function (e) {
+      e.preventDefault();
+      var filters = {
+        timeFrame : this.$('select#time-frame').val() || 'LAST_30_DAYS',
+        minWeight : this.$('input#weight-limit').val() || 30
+      };
+      this.model.trigger('fetchClientGraph', filters);
+    },
+
+    drawGraph : function (data) {
+      this.$('svg.chart').empty();
+
+      var force = d3.layout.force()
+          .gravity(0.05)
+          .distance(300)
+          .charge(-100)
+          .size([this.width, this.height]);
+
+        force
+            .nodes(data.nodes)
+            .links(data.links)
+            .start();
+
+        var link = this.svgChart.selectAll(".link")
+            .data(data.links)
+          .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function(d) {
+              return (d.value / 50);
+            });
+
+        var node = this.svgChart.selectAll('.node')
+            .data(data.nodes)
+          .enter().append('g')
+            .attr('class', 'node')
+            .call(force.drag)
+            .on('mousedown', function(d) {
+              d.fixed = true;
+              var selectedNode = d3.select(this).select('circle')
+                .classed('sticky', true);
+            });
+
+        node.append('circle')
+          .attr('r', 10);
+
+        node.append('text')
+            .attr('dx', 16)
+            .attr('dy', '.95em')
+            .text(function(d) { return d.name; });
+
+        force.on('tick', function() {
+          link.attr('x1', function(d) { return d.source.x; })
+              .attr('y1', function(d) { return d.source.y; })
+              .attr('x2', function(d) { return d.target.x; })
+              .attr('y2', function(d) { return d.target.y; });
+
+          node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+        });
+    }
+  });
+
+  return {
+    model : clientGraphModel,
+    view : clientGraphView
+  };
 });
